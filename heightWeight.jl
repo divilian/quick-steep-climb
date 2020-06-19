1ecfffc73f94f26e913f2f3ee3d1e361d6e805d3
Stephen <stephen@umw.edu>
2020-06-19 15:39:29 -0400

    Finish "five ways" section.

--- heightWeight.jl
+++ heightWeight.jl
@@ -1,19 +1,19 @@
 
 using Plots
-gr()
+pyplot()
 
 
-plot([0,210],[0,74],legend=false,xtickfontsize=16,ytickfontsize=16,guidefontsize=22,color="black",arrow=:closed)
+plot([0,210],[0,74],legend=false,xtickfontsize=16,ytickfontsize=16,guidefontsize=22,color="black",arrow=(1,1))
 xlims!((0,350))
 ylims!((0,350))
 xlabel!("weight (pounds)")
 ylabel!("height (inches)")
 savefig("vector.png")
 
-plot([0,210],[0,74],xaxis=false,yaxis=false,guidefontsize=14,legend=false,color="black",arrow=:closed)
-plot!([-150,60],[80,154],color="black",arrow=:closed)
-plot!([-50,160],[-96,-22],color="black",arrow=:closed)
-plot!([-300,-90],[-300,-226],color="black",arrow=:closed)
+plot([0,210],[0,74],xaxis=false,yaxis=false,guidefontsize=14,legend=false,color="black",arrow=(1,1))
+plot!([-150,60],[80,154],color="black",arrow=(1,1))
+plot!([-50,160],[-96,-22],color="black",arrow=(1,1))
+plot!([-300,-90],[-300,-226],color="black",arrow=(1,1))
 plot!([0,0],[-300,300],color="black",linewidth=1)
 plot!([-300,300],[0,0],color="black",linewidth=1)
 xlims!((-300,300))
