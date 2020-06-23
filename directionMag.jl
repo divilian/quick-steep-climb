23800e399249534a465bcdb4d3abc6579db40bb9
Stephen <stephen@umw.edu>
2020-06-23 10:36:43 -0400

    (Darken vector.)

--- directionMag.jl
+++ directionMag.jl
@@ -2,7 +2,7 @@
 using Plots
 pyplot()
 
-plot([0,9],[0,21],legend=false,xtickfontsize=16,ytickfontsize=16,guidefontsize=22,color="black",arrow=(1,1),size=(400,400))
+plot([0,9],[0,21],legend=false,xtickfontsize=16,ytickfontsize=16,guidefontsize=22,color="black",linewidth=2,arrow=(1,1),size=(400,400))
 θ = LinRange(0, atan(21,9), 50)
 plot!(12*cos.(θ), 12*sin.(θ),color="black",arrow=(1,1))
 annotate!((11,8,Plots.text("\$θ\$",20)))
