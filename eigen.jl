944d8ac11d3b0bccf3d092b0bbbf245eb2f445ad
Stephen <stephen@umw.edu>
2020-09-29 13:13:07 -0400

    Add third eigenvector plot.

--- eigen.jl
+++ eigen.jl
@@ -63,13 +63,13 @@ savefig("crazytrans3.png")
 
 
 # input vector
-plot([0,-2],[0,1],legend=false,xtickfontsize=16,ytickfontsize=16,guidefontsize=22,color="black",linewidth=2,arrow=(1,1),size=(400,400))
-annotate!(-2,2,Plots.text("(-2,1)",13))
+plot([0,-1],[0,.5],legend=false,xtickfontsize=16,ytickfontsize=16,guidefontsize=22,color="black",linewidth=2,arrow=(1,1),size=(400,400))
+annotate!(.4,.9,Plots.text("\$(-1,\\frac{1}{2})\$",13))
 
 # output vector
-plot!([0,-4],[0,2],color="black",linewidth=2,arrow=(1,1),linestyle=:dash)
-annotate!(-4,3,text("(-4,-2)",13))
-annotate!(-4,.5,text("boom!",14))
+plot!([0,-2],[0,1],color="black",linewidth=2,arrow=(1,1),linestyle=:dash)
+annotate!(-3.4,1.3,text("(-2,-1)",13))
+annotate!(-3,.25,text("boom!",10))
 
 # window dressing
 xlims!((-8,8))
@@ -81,6 +81,27 @@ plot!([-8,8],[0,0],color="black",linewidth=1,style=:dot)
 #plot!(4.5*cos.(θ), 4.5*sin.(θ),color="black",arrow=(1,.8),linestyle=:dot)
 
 savefig("eigentrans1.png")
+
+
+# input vector
+plot([0,-2],[0,1],legend=false,xtickfontsize=16,ytickfontsize=16,guidefontsize=22,color="black",linewidth=2,arrow=(1,1),size=(400,400))
+annotate!(-2,2,Plots.text("(-2,1)",13))
+
+# output vector
+plot!([0,-4],[0,2],color="black",linewidth=2,arrow=(1,1),linestyle=:dash)
+annotate!(-4,3,text("(-4,-2)",13))
+annotate!(-4,.5,text("boom!",13))
+
+# window dressing
+xlims!((-8,8))
+ylims!((-8,8))
+plot!([0,0],[-8,8],color="black",linewidth=1,style=:dot)
+plot!([-8,8],[0,0],color="black",linewidth=1,style=:dot)
+
+#θ = LinRange(atan(5,-3), atan(6.5,-9.5), 50)
+#plot!(4.5*cos.(θ), 4.5*sin.(θ),color="black",arrow=(1,.8),linestyle=:dot)
+
+savefig("eigentrans2.png")
 
 
 # input vector
@@ -101,4 +122,4 @@ plot!([-8,8],[0,0],color="black",linewidth=1,style=:dot)
 #θ = LinRange(atan(5,-3), atan(6.5,-9.5), 50)
 #plot!(4.5*cos.(θ), 4.5*sin.(θ),color="black",arrow=(1,.8),linestyle=:dot)
 
-savefig("eigentrans2.png")
+savefig("eigentrans3.png")
